eecd3f2b4cc1c7e7bd471e9b2b39b8773dac0aef
divay0710 <116971617+divay0710@users.noreply.github.com>
2023-05-16 15:00:50 +0530

    Update locals.tf

--- module/locals.tf
+++ module/locals.tf
@@ -8,7 +8,7 @@ locals {
   ]
   app_commands = [
     "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/raghudevopsb72/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/divay0710/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }
 
