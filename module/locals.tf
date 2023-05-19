8b2e88879aa0742ac73bafd7bcb27d7fbef1b074
divay0710 <vimaldivay07@gmail.com>
2023-05-19 23:45:14 +0530

    terraform

--- module/locals.tf
+++ module/locals.tf
@@ -10,5 +10,19 @@ locals {
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/divay0710/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
+
+ db_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name      = "${var.component_name}-${var.env}"
+    Monitor   = "true"
+    component = var.component_name
+    env       = var.env
+  }
 }
+
+
+
+
 
