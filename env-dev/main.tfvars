a79f4c78713650222e9f32796799a1d95f2f4810
divay0710 <vimaldivay07@gmail.com>
2023-05-30 00:35:01 +0530

    as

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,6 +1,115 @@
-env = "dev"
+env              = "dev"
+bastion_cidr     = ["172.31.16.0/20"]
+default_vpc_id   = "vpc-05011095d37a49fe7"
+default_vpc_cidr = "172.31.0.0/16"
+default_vpc_rtid = "rtb-08b4e6684ade98825"
+kms_arn          = "arn:aws:kms:us-east-1:311804564652:key/79344515-5cb2-4249-9651-3b8c1cf1a6b2"
+
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
+    subnets = {
+      public = {
+        name       = "public"
+        cidr_block = ["10.0.0.0/24", "10.0.1.0/24"]
+        azs        = ["us-east-1a", "us-east-1b"]
+      }
+      web = {
+        name       = "web"
+        cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
+        azs        = ["us-east-1a", "us-east-1b"]
+      }
+      app = {
+        name       = "app"
+        cidr_block = ["10.0.4.0/24", "10.0.5.0/24"]
+        azs        = ["us-east-1a", "us-east-1b"]
+      }
+      db = {
+        name       = "db"
+        cidr_block = ["10.0.6.0/24", "10.0.7.0/24"]
+        azs        = ["us-east-1a", "us-east-1b"]
+      }
+    }
+  }
+}
+
+app = {
+  frontend = {
+    name             = "frontend"
+    instance_type    = "t3.small"
+    subnet_name      = "web"
+    allow_app_cidr   = "public"
+    desired_capacity = 2
+    max_size         = 10
+    min_size         = 2
+  }
+  catalogue = {
+    name             = "catalogue"
+    instance_type    = "t3.small"
+    subnet_name      = "app"
+    allow_app_cidr   = "web"
+    desired_capacity = 2
+    max_size         = 10
+    min_size         = 2
+  }
+  //  cart = {
+  //    name          = "cart"
+  //    instance_type = "t3.small"
+  //    subnet_name   = "app"
+  //  }
+  //  user = {
+  //    name          = "user"
+  //    instance_type = "t3.small"
+  //    subnet_name   = "app"
+  //  }
+  //  shipping = {
+  //    name          = "shipping"
+  //    instance_type = "t3.small"
+  //    subnet_name   = "app"
+  //  }
+  //  payment = {
+  //    name          = "payment"
+  //    instance_type = "t3.small"
+  //    subnet_name   = "app"
+  //  }
+}
+
+
+docdb = {
+  main = {
+    subnet_name    = "db"
+    allow_db_cidr  = "app"
+    engine_version = "4.0.0"
+    instance_count = 1
+    instance_class = "db.t3.medium"
+  }
+}
+
+rds = {
+  main = {
+    subnet_name    = "db"
+    allow_db_cidr  = "app"
+    engine_version = "5.7.mysql_aurora.2.11.2"
+    instance_count = 1
+    instance_class = "db.t3.small"
+  }
+}
+
+elasticache = {
+  main = {
+    subnet_name             = "db"
+    allow_db_cidr           = "app"
+    engine_version          = "6.x"
+    replicas_per_node_group = 1
+    num_node_groups         = 1
+    node_type               = "cache.t3.micro"
+  }
+}
+
+rabbitmq = {
+  main = {
+    subnet_name   = "db"
+    allow_db_cidr = "app"
+    instance_type = "t3.small"
   }
 }
--- main.tf
+++ main.tf
@@ -3,6 +3,106 @@ module "vpc" {
 
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  tags       = local.tags
-  env        = var.env
+  subnets    = each.value["subnets"]
+
+  tags             = local.tags
+  env              = var.env
+  default_vpc_id   = var.default_vpc_id
+  default_vpc_cidr = var.default_vpc_cidr
+  default_vpc_rtid = var.default_vpc_rtid
 }
+
+
+//module "app" {
+//  source = "git::https://github.com/divay0710/tf-module-app.git"
+//
+//  for_each         = var.app
+//  instance_type    = each.value["instance_type"]
+//  name             = each.value["name"]
+//  desired_capacity = each.value["desired_capacity"]
+//  max_size         = each.value["max_size"]
+//  min_size         = each.value["min_size"]
+//
+//  env          = var.env
+//  bastion_cidr = var.bastion_cidr
+//  tags         = local.tags
+//
+//  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+//  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+//  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+//}
+
+
+
+
+
+
+module "docdb" {
+  source = "git::https://github.com/divay0710/tf-module-docdb.git"
+
+  for_each       = var.docdb
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
+
+
+  tags    = local.tags
+  env     = var.env
+  vpc_id  = local.vpc_id
+  kms_arn = var.kms_arn
+}
+
+
+module "rds" {
+  source = "git::https://github.com/divay0710/tf-module-rds.git"
+
+  for_each       = var.rds
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
+
+
+  tags    = local.tags
+  env     = var.env
+  vpc_id  = local.vpc_id
+  kms_arn = var.kms_arn
+}
+
+module "elasticache" {
+  source = "git::https://github.com/divay0710/tf-module-elasticache.git"
+
+  for_each                = var.elasticache
+  subnets                 = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_db_cidr           = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version          = each.value["engine_version"]
+  replicas_per_node_group = each.value["replicas_per_node_group"]
+  num_node_groups         = each.value["num_node_groups"]
+  node_type               = each.value["node_type"]
+
+
+  tags    = local.tags
+  env     = var.env
+  vpc_id  = local.vpc_id
+  kms_arn = var.kms_arn
+}
+
+module "rabbitmq" {
+  source = "git::https://github.com/divay0710/tf-module-amazon-mq.git"
+
+  for_each      = var.rabbitmq
+  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  instance_type = each.value["instance_type"]
+
+
+  tags         = local.tags
+  env          = var.env
+  vpc_id       = local.vpc_id
+  kms_arn      = var.kms_arn
+  bastion_cidr = var.bastion_cidr
+}
+
