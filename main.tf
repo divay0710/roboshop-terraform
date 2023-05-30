371cb72c688941da572c45d6d7ba95df9f2118d8
divay0710 <vimaldivay07@gmail.com>
2023-05-30 19:09:06 +0530

    as

--- main.tf
+++ main.tf
@@ -13,30 +13,6 @@ module "vpc" {
 }
 
 
-//module "app" {
-//  source = "git::https://github.com/divay0710/tf-module-app.git"
-//
-//  for_each         = var.app
-//  instance_type    = each.value["instance_type"]
-//  name             = each.value["name"]
-//  desired_capacity = each.value["desired_capacity"]
-//  max_size         = each.value["max_size"]
-//  min_size         = each.value["min_size"]
-//
-//  env          = var.env
-//  bastion_cidr = var.bastion_cidr
-//  tags         = local.tags
-//
-//  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-//  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-//  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
-//}
-
-
-
-
-
-
 module "docdb" {
   source = "git::https://github.com/divay0710/tf-module-docdb.git"
 
@@ -106,3 +82,38 @@ module "rabbitmq" {
   bastion_cidr = var.bastion_cidr
 }
 
+module "alb" {
+  source = "git::https://github.com/divay0710/tf-module-alb.git"
+
+  for_each       = var.alb
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_alb_cidr = each.value["name"] == "public" ? ["0.0.0.0/0"] : lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null)
+  name           = each.value["name"]
+  internal       = each.value["internal"]
+
+
+  tags   = local.tags
+  env    = var.env
+  vpc_id = local.vpc_id
+}
+
+module "app" {
+  depends_on = [module.vpc, module.docdb, module.rds, module.elasticache, module.rabbitmq, module.alb]
+  source     = "git::https://github.com/divay0710/tf-module-app.git"
+
+  for_each         = var.app
+  instance_type    = each.value["instance_type"]
+  name             = each.value["name"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size         = each.value["max_size"]
+  min_size         = each.value["min_size"]
+  app_port         = each.value["app_port"]
+
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+
+  env          = var.env
+  bastion_cidr = var.bastion_cidr
+  tags         = local.tags
+}
--- vars.tf
+++ vars.tf
@@ -1,3 +1,12 @@
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "name" {}
 variable "env" {}
-variable "vpc" {}
-
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
+variable "bastion_cidr" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "tags" {}
+variable "app_port" {}
