16095ce4987ab5991e0888954e4116a1058ff453
divay0710 <vimaldivay07@gmail.com>
2023-06-01 20:08:42 +0530

    ad

--- main.tf
+++ main.tf
@@ -83,9 +83,9 @@ module "rabbitmq" {
 }
 
 module "alb" {
-  source = "git::https://github.com/divay0710/tf-module-alb.git"
+  source = "git::https://github.com/raghudevopsb72/tf-module-alb.git"
 
-   for_each       = var.alb
+  for_each       = var.alb
   subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_alb_cidr = each.value["name"] == "public" ? ["0.0.0.0/0"] : lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null)
   name           = each.value["name"]
